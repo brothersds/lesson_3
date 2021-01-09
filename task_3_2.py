78302d84bddd40c29b4f5adb76d4ff052e83c019
Dmitry <brothersds@ngs.ru>
2021-01-09 22:12:47 +0300

    completed task2

--- task_3_2.py
+++ task_3_2.py
@@ -6,7 +6,10 @@
 
 
 def my_func(name, surname, year_birth, current_city, user_email, phone_number):
-    return print(f"name - {name}; surname - {surname}; year_birth - {year_birth}; current_city - {current_city}; user_email - {user_email}; phone_number - {phone_number}")
+    return print(f"name - {name}; surname - {surname};\
+    year_birth - {year_birth}; current_city - {current_city};\
+    user_email - {user_email}; phone_number - {phone_number}")
 
 
-my_func(name=Ivan, surname=Petrov, year_birth=1956, current_city=Moscow, user_email=sds@mail.ru, phone_number=2113636)
+my_func(name='Ivan', surname='Petrov', year_birth=1956, current_city='Moscow', user_email='petrov@mail.ru',
+        phone_number=2113636)
